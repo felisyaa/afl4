aae0e21d32cd7755875138b33e3596796da6b262
felisyaa <ffaizal01@student.ciputra.ac.id>
2022-05-24 08:19:46 +0700

    Initial Commit

---
+++ sby/sbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  sbyApp.swift
+//  sby
+//
+//  Created by Felisya on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct sbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
